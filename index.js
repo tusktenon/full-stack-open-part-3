d2a0f05f8d9c20b390d47c0f10b4311cce2ec6c8
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-01-16 16:35:33 -0500

    feat: start phonebook backend project

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (request, response) => {
+  response.send('<h1>Phonebook Server</h1>')
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
