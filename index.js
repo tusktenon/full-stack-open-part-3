abe0cbffee47b4f74c6737ee22cc40c8db588a60
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-01-21 20:57:57 -0500

    style: add ESLint

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.patch('/api/persons/:id', (request, response, next) => {
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then(result => response.status(204).end())
+    .then(() => response.status(204).end())
     .catch(error => next(error))
 })
 
